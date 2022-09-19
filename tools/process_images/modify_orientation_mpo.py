7baf0e390a10511d2b8eb89fcc4348bde2412e42
JiuqingDong <111500217+JiuqingDong@users.noreply.github.com>
2022-09-19 11:28:14 +0900

    Update modify_orientation_mpo.py

--- tools/process_images/modify_orientation_mpo.py
+++ tools/process_images/modify_orientation_mpo.py
@@ -14,11 +14,9 @@ def exif_transpose_hand(img, orientation):
     #if not img:
     #    return img
     if orientation == None:
-
         return img
     else:
         image = img.rotate(0, expand=True)
-
     return image
 
 
@@ -67,37 +65,18 @@ def load_image_file(file, mode='RGB'):
     # Load the image with PIL
     img = PIL.Image.open(file)
     if hasattr(PIL.ImageOps, 'exif_transpose'):
-        # Very recent versions of PIL can do exit transpose internally
-        # print(" if hasattr(PIL.ImageOps, 'exif_transpose'):")
         a = img.getexif().get(0x0112)
-        # if a == 1  or a == None:
         print(file, ' = = = = = ', a)
-
-        #img = PIL.ImageOps.exif_transpose(img)
-
         image = exif_transpose_hand(img, a)
     else:
         print("=else=")
-        # Otherwise, do the exif transpose ourselves
-        # image = exif_transpose(img)
 
     image = image.convert(mode)
     #return img
     return np.array(image)
 
-# image_dir = '/Users/Dong/Desktop/project_paper_code_data/Plant_diseases_dataset/Paprika Dataset_v_1.02/images'
-
-# for set in ['train/', 'val/', 'test/']:
-#     image_dir = image_dir0+set
-#     output_dir = output_dir0+set
-
 for root, dirs, files in os.walk(image_dir):
     for file in files:
         filename = os.path.join(root, file)
         img = load_image_file(filename)
-
-        # plt.imshow(img)
         plt.imsave(output_dir+file, img)
-
-        #plt.pause(1)
-        #plt.close()
